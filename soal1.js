459251f2a4c1e2307db82b745d2311ddce07880f
NadhirotulLaily <nadhirotullaily1207@gmail.com>
2023-09-30 19:42:34 +0700

    first commit

---
+++ soal1.js
@@ -0,0 +1,6 @@
+const geometry = require('./geometry');
+
+console.log('Luas Persegi :', geometry.luasPersegi(8));
+console.log('Keliling Persegi :',geometry.kelilingPersegi(3));
+console.log('Luas Persegi Panjang :',geometry.luasPersegiPanjang(10, 4));
+console.log('Keliling Persegi Panjang :',geometry.kelilingPersegiPanjang(5, 6));
